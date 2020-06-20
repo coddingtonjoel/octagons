4c107e5b807c18b19103667caab08b363f2128ee
Joel Coddington <coddingtonjoel@gmail.com>
2020-06-20 13:56:06 -0700

    Compatibility Fix
    added better compatibility on different sized devices and fixed some errors

--- client/src/components/Project.js
+++ client/src/components/Project.js
@@ -17,19 +17,19 @@ const Project = (props) => {
                 <img className="project-img" src={props.img2} alt="" />
                 <img className="project-img" src={props.img3} alt="" />
             </div>
-            <a class="waves-effect btn-flat modal-trigger" href="#modal1">
+            <a className="waves-effect btn-flat modal-trigger" href="#modal1">
                 Click here to view the full project
             </a>
-            <div id="modal1" class="modal">
-                <div class="modal-content">
+            <div id="modal1" className="modal">
+                <div className="modal-content">
                     <iframe
                         className="modal-slides"
                         src="https://docs.google.com/presentation/d/e/2PACX-1vQCuu7Cl93I4urJw8i6TaAZ0-AxFDH-I7y0hB03iFieZ2Me_dD3GmtfqoF2ctw3g3BBxZPH-dgMsv-q/embed?start=false&loop=false&delayms=3000"
-                        frameborder="0"
+                        frameBorder="0"
                         title="Pinterest Playbook"
                         width="950"
                         height="569"
-                        allowfullscreen="true"
+                        allowFullScreen={true}
                         mozallowfullscreen="true"
                         webkitallowfullscreen="true"></iframe>
                 </div>
