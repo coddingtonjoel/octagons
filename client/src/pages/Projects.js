f4dd9244cca5adda481ce4202d89736a1ba94225
Joel Coddington <coddingtonjoel@gmail.com>
2020-08-01 13:21:06 -0700

    Finalization

--- client/src/pages/Projects.js
+++ client/src/pages/Projects.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Project from "../components/Project";
 import projectlist from "../projectlist";
 
 const Projects = () => {
@@ -11,21 +10,182 @@ const Projects = () => {
                 </div>
             </div>
             <div className="projects-container">
-                {projectlist.map((project) => {
-                    return (
-                        <Project
-                            companyLogo={project.companyLogo}
-                            desc={project.desc}
-                            img1={project.img1}
-                            img2={project.img2}
-                            img3={project.img3}
-                            projectSrc={project.projectSrc}
-                            testamonial={project.testamonial}
-                            testamonialAuthor={project.testamonialAuthor}
-                            key={project.desc}
+                {/* IMPAPER */}
+                <div className="project">
+                    <img
+                        className="project-logo"
+                        src={projectlist[0].companyLogo}
+                        alt=""
+                    />
+                    <hr />
+                    <p>{projectlist[0].desc}</p>
+                    <blockquote>
+                        "{projectlist[0].testamonial}"
+                        <br />
+                        <br />
+                        {projectlist[0].testamonialAuthor}
+                    </blockquote>
+                    <div className="project-img-container">
+                        <img
+                            className="project-img"
+                            src={projectlist[0].img1}
+                            alt=""
                         />
-                    );
-                })}
+                        <img
+                            className="project-img"
+                            src={projectlist[0].img2}
+                            alt=""
+                        />
+                        <img
+                            className="project-img"
+                            src={projectlist[0].img3}
+                            alt=""
+                        />
+                    </div>
+                    <a
+                        className="waves-effect btn-flat modal-trigger"
+                        href="#modal1">
+                        View Pinterest Playbook
+                    </a>
+                    <div id="modal1" className="modal">
+                        <div className="modal-content">
+                            <iframe
+                                className="modal-slides"
+                                src="https://docs.google.com/presentation/d/e/2PACX-1vQCuu7Cl93I4urJw8i6TaAZ0-AxFDH-I7y0hB03iFieZ2Me_dD3GmtfqoF2ctw3g3BBxZPH-dgMsv-q/embed?start=false&loop=false&delayms=3000"
+                                frameBorder="0"
+                                title="Pinterest Playbook"
+                                width="950"
+                                height="569"
+                                allowFullScreen={true}
+                                mozallowfullscreen="true"
+                                webkitallowfullscreen="true"></iframe>
+                        </div>
+                    </div>
+                </div>
+                {/* M+B */}
+                <div className="project">
+                    <img
+                        className="project-logo"
+                        src={projectlist[1].companyLogo}
+                        alt=""
+                    />
+                    <hr />
+                    <p>{projectlist[1].desc}</p>
+                    <blockquote>
+                        "{projectlist[1].testamonial}"
+                        <br />
+                        <br />
+                        {projectlist[1].testamonialAuthor}
+                    </blockquote>
+                    <div className="project-img-container">
+                        <img
+                            className="project-img"
+                            src={projectlist[1].img1}
+                            alt=""
+                        />
+                        <img
+                            className="project-img"
+                            src={projectlist[1].img2}
+                            alt=""
+                        />
+                        <img
+                            className="project-img"
+                            src={projectlist[1].img3}
+                            alt=""
+                        />
+                    </div>
+                    <a
+                        className="waves-effect btn-flat modal-trigger"
+                        href="#modal2">
+                        View Content Strategy Guide
+                    </a>
+                    <a
+                        className="waves-effect btn-flat modal-trigger project-external-link"
+                        rel="noopener noreferrer"
+                        target="_blank"
+                        href="https://www.canva.com/design/DAEAYkqZN28/lgVJ17GcnYGlp71ATsNz0g/view?utm_content=DAEAYkqZN28&utm_campaign=designshare&utm_medium=link&utm_source=homepage_design_menu#1">
+                        View Brand Guide
+                    </a>
+                    <a
+                        className="waves-effect btn-flat project-external-link"
+                        href={`${process.env.PUBLIC_URL}/static/mb-mockups.zip`}
+                        download>
+                        Download Mockups
+                    </a>
+                    <div id="modal2" className="modal">
+                        <div className="modal-content">
+                            <iframe
+                                className="modal-slides"
+                                src="https://docs.google.com/presentation/d/e/2PACX-1vSzOIc--OZ_GmgKYenmfEINLVlj-3SCWPYOW9XQ4cOrwlZLbJxC0TUl7tqBzzWuKOZLqkOtqVhp7LyA/embed?start=false&loop=false&delayms=3000"
+                                frameBorder="0"
+                                title="Milk + Brookies Brand Guide"
+                                width="950"
+                                height="569"
+                                allowFullScreen={true}
+                                mozallowfullscreen="true"
+                                webkitallowfullscreen="true"></iframe>
+                        </div>
+                    </div>
+                </div>
+                {/* IVY'S TEA */}
+                <div className="project">
+                    <img
+                        className="project-logo"
+                        src={projectlist[2].companyLogo}
+                        alt=""
+                        style={{ borderRadius: "12px" }}
+                    />
+                    <hr />
+                    <p>{projectlist[2].desc}</p>
+                    <blockquote>
+                        "{projectlist[2].testamonial}"
+                        <br />
+                        <br />
+                        {projectlist[2].testamonialAuthor}
+                    </blockquote>
+                    <div className="project-img-container">
+                        <img
+                            className="project-img"
+                            src={projectlist[2].img1}
+                            alt=""
+                        />
+                        <img
+                            className="project-img"
+                            src={projectlist[2].img2}
+                            alt=""
+                        />
+                        <img
+                            className="project-img"
+                            src={projectlist[2].img3}
+                            alt=""
+                        />
+                    </div>
+                    <a
+                        className="waves-effect btn-flat modal-trigger"
+                        href="#modal3">
+                        View Instagram Ad Campaign
+                    </a>
+                    <a
+                        className="waves-effect btn-flat project-external-link"
+                        href={`${process.env.PUBLIC_URL}/static/ivys-tea-mockup.pdf`}
+                        download>
+                        Download Mockups
+                    </a>
+                    <div id="modal3" className="modal">
+                        <div className="modal-content">
+                            <iframe
+                                className="modal-slides"
+                                src="https://docs.google.com/presentation/d/e/2PACX-1vRuWsGLAqMzcFRypUDJY8JhK97SXwy93CBakRiAAAcD9tmLhHN5pP1t5oGNJD28OELLjD859DAZHsEq/embed?start=false&loop=false&delayms=3000"
+                                frameBorder="0"
+                                title="Ivy's Tea Instagram Campaign"
+                                width="950"
+                                height="569"
+                                allowFullScreen={true}
+                                mozallowfullscreen="true"
+                                webkitallowfullscreen="true"></iframe>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
--- client/src/projectlist.js
+++ client/src/projectlist.js
@@ -2,6 +2,14 @@ import tyche from "./images/tyche_gami/logo.png";
 import tyche1 from "./images/tyche_gami/img1.jpg";
 import tyche2 from "./images/tyche_gami/img2.jpg";
 import tyche3 from "./images/tyche_gami/img3.jpg";
+import mb from "./images/milk-and-brookies/milk-and-brookies.png";
+import mb1 from "./images/milk-and-brookies/mb1.png";
+import mb2 from "./images/milk-and-brookies/mb2.png";
+import mb3 from "./images/milk-and-brookies/mb3.png";
+import ivy from "./images/ivys-tea/ivy.png";
+import ivy1 from "./images/ivys-tea/ivy1.png";
+import ivy2 from "./images/ivys-tea/ivy2.png";
+import ivy3 from "./images/ivys-tea/ivy3.png";
 
 const projectlist = [
     {
@@ -14,6 +22,26 @@ const projectlist = [
         img1: tyche1,
         img2: tyche2,
         img3: tyche3,
+    },
+    {
+        companyLogo: mb,
+        desc:
+            "The Octagons team built Milk + Brookies a squarespace website and mockup, a brand guide, and a content strategy guide to help the former Shark Tank business reimagine their brand.",
+        testamonial: "N/A",
+        testamonialAuthor: "N/A",
+        img1: mb1,
+        img2: mb2,
+        img3: mb3,
+    },
+    {
+        companyLogo: ivy,
+        desc:
+            "The Octagons team helped create Ivy's Tea a detailed website mockup, a guide for an instagram ad, and pitched a advertizing campaign to rethink the company's incorporation of hip hop and tea.",
+        testamonial: "N/A",
+        testamonialAuthor: "N/A",
+        img1: ivy1,
+        img2: ivy2,
+        img3: ivy3,
     },
 ];
 
